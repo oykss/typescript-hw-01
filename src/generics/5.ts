f2ab71e1ecf23e3aa5a985fc0944be2e4430eca5
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-01-23 14:05:06 +0200

    Update 5.ts

--- src/generics/5.ts
+++ src/generics/5.ts
@@ -5,7 +5,7 @@ export enum UserRole {
 }
 
 const RoleDescription: Record<UserRole, string> = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
